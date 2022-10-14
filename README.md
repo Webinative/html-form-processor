cb9e6fe8617b711c7242f685657a84e8761b1234
Magesh Ravi <magesh@webinative.com>
2022-10-14 20:42:20 +0530

    init
    - initialise repository with flask app

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# html-form-processor
-A flask app that prints submitted form-data. Helpful for frontend beginners learning form elements.
+# HTML Form processor
+
+A flask app that prints submitted form-data. Helpful for beginners to understand how HTML forms submit data to backend.
+
+## Requirements
+
+1. Python >= 3.8
+2. VirtualEnvWrapper
+
+See this [blogpost](https://www.webinative.com/blog/setting-up-python-virtual-environments/) to setup virtualenvwrapper.
+
+## How to run this app
+
+Follow the instructions below,
+
+```sh
+cd ~/Dev
+git clone git@github.com:Webinative/html-form-processor.git
+cd html-form-processor
+
+# create a new virtual environment
+mkvirtualenv flask_app
+
+pip3 install Flask
+
+# TODO: Edit templates/form.html to place your form.
+
+# run the flask app
+flask --app flask_app --debug run
+```
---
+++ flask_app/__init__.py
@@ -0,0 +1,38 @@
+import os
+from flask import Flask, render_template, request
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path,'db.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route('/', methods=['GET'])
+    def render_form():
+        return render_template("form.html")
+
+    @app.route('/', methods=['POST'])
+    def process_form():
+        postdata={}
+        for val in request.form.to_dict(flat=False):
+            postdata[val] = request.form.getlist(val)
+
+        return render_template('index.html', postdata=postdata)
+
+    return app
